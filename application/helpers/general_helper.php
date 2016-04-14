07daa775cdac428330b4abd31a3e56b260c3959d
Adam Prasetia <adam.prasetia@kompas.com>
2016-04-14 15:16:40 +0700

    fix age

--- application/helpers/general_helper.php
+++ application/helpers/general_helper.php
@@ -340,3 +340,13 @@ function get_mm(){
     );
     return $data;
 }
+function calcutate_age($dob){
+    $dob = date("Y-m-d",strtotime($dob));
+
+    $dobObject = new DateTime($dob);
+    $nowObject = new DateTime();
+
+    $diff = $dobObject->diff($nowObject);
+
+    return $diff->y;
+}
