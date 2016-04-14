6d09676c8000e1bfde26541991627bfd67444864
Adam Prasetia <adam.prasetia@kompas.com>
2016-04-14 12:04:05 +0700

    fix import module

--- application/modules/import/controllers/Import.php
+++ application/modules/import/controllers/Import.php
@@ -31,13 +31,18 @@ class Import extends MY_Controller {
 				$i=3;
 				while(trim($active_sheet->getCell('A'.$i)->getValue())<>''){
 					$data[] = array(
-						'fullname'=>trim($active_sheet->getCell('B'.$i)->getValue())
-						,'nickname'=>trim($active_sheet->getCell('C'.$i)->getValue())
-						,'dob'=>trim($active_sheet->getCell('F'.$i)->getValue()).'-'.trim($active_sheet->getCell('E'.$i)->getValue()).'-'.trim($active_sheet->getCell('D'.$i)->getValue())
-						,'tlp'=>trim($active_sheet->getCell('G'.$i)->getValue())
-						,'email'=>trim($active_sheet->getCell('H'.$i)->getValue())
-						,'date_create'=>date('Y-m-d H:i:s')
-						,'user_create'=>$this->user_login['id']
+						'mop_id'=>trim($active_sheet->getCell('B'.$i)->getValue()),
+						'username'=>trim($active_sheet->getCell('C'.$i)->getValue()),
+						'fullname'=>trim($active_sheet->getCell('D'.$i)->getValue()),
+						'nickname'=>trim($active_sheet->getCell('E'.$i)->getValue()),
+						'dob'=>trim($active_sheet->getCell('H'.$i)->getValue()).'-'.trim($active_sheet->getCell('G'.$i)->getValue()).'-'.trim($active_sheet->getCell('F'.$i)->getValue()),
+						'tlp'=>trim($active_sheet->getCell('I'.$i)->getValue()),
+						'email'=>trim($active_sheet->getCell('J'.$i)->getValue()),
+						'fb'=>trim($active_sheet->getCell('K'.$i)->getValue()),
+						'tw'=>trim($active_sheet->getCell('L'.$i)->getValue()),
+						'ins'=>trim($active_sheet->getCell('M'.$i)->getValue()),
+						'date_create'=>date('Y-m-d H:i:s'),
+						'user_create'=>$this->user_login['id'],
 					);
 					$i++;
 				}
